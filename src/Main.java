e8c8b5223bf6ce0aa9f6ebd8b018df0ce6b53097
lidia <lidokha.iordan@gmail.com>
2025-05-14 18:54:49 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    double EPS = 1e-9;
+    public static double calculate(int n, int depth) {
+        if (depth == 0) {
+            // Базовый случай: дальше корней нет
+            return 0.0;
         }
+        double inner = calculate(n + 1, depth - 1);
+        System.out.println("Calc sqrt(" + (6.0 + n) + " + " + (n + 2) + " * in_sqrt==" + inner + ")" );
+        return Math.sqrt((6.0 + n) + (n + 2) * inner);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите глубину вложенности N: ");
+        int depth = scanner.nextInt();
+        scanner.close();
+
+        double result = calculate(0, depth);
+        System.out.printf(
+                "При глубине вложенности %d значение выражения ≈ %.10f%n",
+                depth, result
+        );
     }
 }
